d0695bb5241c026ed491822acce9e891b437678a
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-19 10:50:39 +0100

    Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { useState } from "react";
+import API from "../api";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+const Login = ({ setUser }) => {
+  const [form, setForm] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await API.post("/auth/login", form);
+      localStorage.setItem("token", res.data.token);
+      setUser(res.data);
+      navigate("/");
+    } catch (err) {
+      alert("Identifiants incorrects !");
+    }
+  };
+
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <input placeholder="Email" type="email" onChange={(e) => setForm({ ...form, email: e.target.value })} required />
+      <input placeholder="Mot de passe" type="password" onChange={(e) => setForm({ ...form, password: e.target.value })} required />
+      <button type="submit">Se connecter</button>
+    </form>
+  );
+};
 
-export default Login
+export default Login;
