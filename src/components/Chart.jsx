3070a943a3defd559b8a1c3962db1482b5175962
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-19 10:13:51 +0100

    Chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,4 +1,7 @@
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 import { Bar } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend); // ✅ Enregistrement
 
 const Chart = ({ transactions }) => {
   const income = transactions.filter(t => t.type === "income").reduce((sum, t) => sum + Number(t.amount), 0);
